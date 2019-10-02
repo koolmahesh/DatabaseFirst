1be63c0eeecaa54f566487e4cd431b8eae483a9e
koolmahesh <maheshkumar.7488@gmail.com>
2019-10-02 22:26:12 +0530

    wedw

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conext = new DatabaseFirstDemoEntities();
+            var table = new table_1()
+            {
+                Body = "body",
+                DatePublished = "ds",
+                Title = "title",
+                PostID= 1 
+
+            };
+            conext.table_1.Add(table );
+            conext.SaveChanges();
+        }
+    }
+}
